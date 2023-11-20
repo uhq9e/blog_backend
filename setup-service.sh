b55c5c085c56b06edd0e50576ba0ffb0a9b393c4
iitenkie <846708308gm@gmail.com>
2023-11-20 21:25:09 +0800

    upd

--- setup-service.sh
+++ setup-service.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 SERVICE_PATH="/etc/systemd/system/blog_backend.service"
+SERVICE_DIR_PATH="$SERVICE_PATH.d"
+ENV_FILE_PATH="$SERVICE_DIR_PATH/myenv.conf"
 EXECUTABLE_PATH="/root/backend/blog_backend"
+
+mkdir -p $SERVICE_DIR_PATH
 
 if [ ! -e $SERVICE_PATH ]; then
     touch $SERVICE_PATH
+fi
+
+if [ ! -e $ENV_FILE_PATH ]; then
+    touch $ENV_FILE_PATH
 fi
 
 read -e -p 'DATABASE_URL: ' database_url
@@ -21,11 +29,15 @@ Restart=always
 RestartSec=10
 User=root
 ExecStart=$EXECUTABLE_PATH
-Environment=DATABASE_URL="$database_url"
-Environment=JWT_SIGNING_KEY="$jwt_signing_key"
 
 [Install]
 WantedBy=multi-user.target
+EOM
+
+cat >$ENV_FILE_PATH <<-EOM
+[Service]
+Environment="DATABASE_URL=$database_url"
+Environment="JWT_SIGNING_KEY=$jwt_signing_key"
 EOM
 
 echo "已存储服务到 $SERVICE_PATH"
